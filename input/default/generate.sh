ab48023c788e49f457a07f0b6fe84db17950579a
hillsboro <venkat72@gmail.com>
2021-01-31 16:50:14 -0800

    exclude repeaterbook

--- input/default/generate.sh
+++ input/default/generate.sh
@@ -4,11 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 OUTPUT=${OUTPUT:-$DIR/../../OUTPUT}
 python -m dzcb \
     --pnwdigital \
-#    --seattledmr \
     --default-k7abd \
-#    --repeaterbook-proximity-csv "$DIR/prox.csv" \
     --k7abd $DIR/k7abd \
     --farnsworth-template-json "$DIR/example-md-uv380.json" \
-#    --scanlists-json "$DIR/scanlists.json" \
     --order-json "$DIR/order.json" \
 $OUTPUT/$(basename "$DIR")
