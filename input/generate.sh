d0fbed9d24b007ea17e7ddc56ae6982b7a1705d2
hillsboro <venkat72@gmail.com>
2021-02-07 09:56:17 -0800

    tweak prox.csv

---
+++ input/generate.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+OUTPUT=${OUTPUT:-$DIR/../../OUTPUT}
+python -m dzcb \
+    --pnwdigital \
+    --seattledmr \
+    --default-k7abd \
+    --repeaterbook-state washington oregon \
+    --repeaterbook-proximity-csv "$DIR/prox.csv" \
+    --k7abd $DIR/k7abd \
+    --farnsworth-template-json "$DIR/example-md-uv380.json" \
+    --scanlists-json "$DIR/scanlists.json" \
+    --order-json "$DIR/order.json" \
+$OUTPUT/$(basename "$DIR")
