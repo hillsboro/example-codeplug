4363dfaffe087755bf3f84902929fb1ac1ad1009
hillsboro <venkat72@gmail.com>
2021-02-09 16:57:28 -0800

    changed TG names, adjusted timeslots

--- input/default/generate.sh
+++ input/default/generate.sh
@@ -3,12 +3,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 OUTPUT=${OUTPUT:-$DIR/../../OUTPUT}
 python -m dzcb \
-    --pnwdigital \
-    --seattledmr \
     --default-k7abd \
     --repeaterbook-state washington oregon \
     --repeaterbook-proximity-csv "$DIR/prox.csv" \
     --k7abd $DIR/k7abd \
+    --pnwdigital \
     --farnsworth-template-json "$DIR/example-md-uv380.json" \
     --scanlists-json "$DIR/scanlists.json" \
     --order-json "$DIR/order.json" \
