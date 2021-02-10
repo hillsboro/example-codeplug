0f7588060f537f5201d0cab8228317f4e5ff4b2d
hillsboro <venkat72@gmail.com>
2021-02-10 11:34:15 -0800

    remove pnwd experiment

--- input/default/generate.sh
+++ input/default/generate.sh
@@ -3,11 +3,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 OUTPUT=${OUTPUT:-$DIR/../../OUTPUT}
 python -m dzcb \
-    --default-k7abd \
     --repeaterbook-state washington oregon \
     --repeaterbook-proximity-csv "$DIR/prox.csv" \
     --k7abd $DIR/k7abd \
-    --pnwdigital \
+    --default-k7abd \
     --farnsworth-template-json "$DIR/example-md-uv380.json" \
     --scanlists-json "$DIR/scanlists.json" \
     --order-json "$DIR/order.json" \
