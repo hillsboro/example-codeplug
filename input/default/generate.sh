5c09aed6ad789665010d3a34213c5b4a36293d83
hillsboro <venkat72@gmail.com>
2021-02-12 11:33:29 -0800

    removed Parrot to fix build

--- input/default/generate.sh
+++ input/default/generate.sh
@@ -3,8 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 OUTPUT=${OUTPUT:-$DIR/../../OUTPUT}
 python -m dzcb \
-    --repeaterbook-state washington oregon \
-    --repeaterbook-proximity-csv "$DIR/prox.csv" \
+   # --repeaterbook-state washington oregon \
+   # --repeaterbook-proximity-csv "$DIR/prox.csv" \
     --k7abd $DIR/k7abd \
     --default-k7abd \
     --farnsworth-template-json "$DIR/example-md-uv380.json" \
