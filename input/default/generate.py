580fb76392069fc56b975ffafdaca1ee66a6c902
hillsboro <venkat72@gmail.com>
2021-03-17 10:19:26 -0700

    edit generate.py options

--- input/default/generate.py
+++ input/default/generate.py
@@ -13,18 +13,19 @@ output = Path(os.environ.get("OUTPUT") or (cp_dir / ".." / ".." / "OUTPUT"))
 
 CodeplugRecipe(
     source_pnwdigital=True,
-    source_seattledmr=True,
+    source_seattledmr=False,
     source_default_k7abd=True,
     source_k7abd=[(cp_dir / "k7abd")],
     source_repeaterbook_proximity=cp_dir / "prox.csv",
     repeaterbook_states=["washington", "oregon"],
-    repeaterbook_name_format='{Callsign} {Nearest City} {Landmark}',
+    repeaterbook_name_format='{Landmark} {Frequency}',
     scanlists_json=cp_dir / "scanlists.json",
+    include=cp_dir / "include.csv",
     exclude=cp_dir / "exclude.csv",
     order=cp_dir / "order.csv",
     replacements=cp_dir / "replacements.csv",
     output_anytone=True,
     output_dmrconfig=[(cp_dir / "example-d878uv.conf")],
     output_farnsworth=[(cp_dir / "example-md-uv380.json")],
-    output_gb3gf=True
+    output_gb3gf=False
 ).generate(output / cp_dir.name)
